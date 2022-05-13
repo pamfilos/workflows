afa0a2bbec5941734acc725fe643b3cd8db619d9
Ernesta <ernesta.petraityte@yahoo.com>
2022-05-13 14:13:33 +0200

    Cleanup: convert html subscripts to latex (#17)
    * Added html subscripts convertion to latex expressions
    * Added tests
    * ref: cern-sis/issues-scoap3#40
    
    Co-authored-by: Harris Tzovanakis <me@drjova.com>

--- dags/common/cleanup.py
+++ dags/common/cleanup.py
@@ -1,2 +1,11 @@
 def clean_whitespace_characters(input):
     return " ".join(input.split())
+
+def convert_html_subsripts_to_latex(input):
+    from re import sub
+
+    input = sub("<sub>(.*?)</sub>", r"$_{\1}$", input)
+    input = sub("<inf>(.*?)</inf>", r"$_{\1}$", input)
+    input = sub("<sup>(.*?)</sup>", r"$^{\1}$", input)   
+
+    return input
--- tests/units/common/test_cleanup.py
+++ tests/units/common/test_cleanup.py
@@ -1,5 +1,5 @@
 import pytest
-from common.cleanup import clean_whitespace_characters
+from common.cleanup import clean_whitespace_characters, convert_html_subsripts_to_latex
 
 expected_string = "Simple input with spaces"
 string = " Simple input with spaces "
@@ -26,3 +26,37 @@ string_with_new_lines = " Simple   input  \n  with  \n spaces "
 def test_clean_whitespace_characters(test_input, expected):
     output = clean_whitespace_characters(test_input)
     assert output == expected
+
+expected_string_sup = "$^{3}$"
+sup = "<sup>3</sup>"
+expected_string_sub = "$_{3}$"
+sub = "<sub>3</sub>"
+expected_string_inf = "$_{3}$"
+inf = "<inf>3</inf>"
+
+expected_string_sup_extended = '<p content-type="scoap3">Article funded by SCOAP$^{3}$</p>'
+sup_extended  = '<p content-type="scoap3">Article funded by SCOAP<sup>3</sup></p>'
+expected_string_sub_extended  = '<p content-type="scoap3">Article funded by SCOAP$_{3}$</p>'
+sub_extended  = '<p content-type="scoap3">Article funded by SCOAP<sub>3</sub></p>'
+expected_string_inf_extended  = '<p content-type="scoap3">Article funded by SCOAP$_{3}$</p>'
+inf_extended  = '<p content-type="scoap3">Article funded by SCOAP<inf>3</inf></p>'
+
+no_tags = '<p content-type="scoap3">Article funded by SCOAP</p>'
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        pytest.param(sup, expected_string_sup, id="test_convert_html_subsripts_to_latex_sup"),
+        pytest.param(sub, expected_string_sub, id="test_convert_html_subsripts_to_latex_sub"),
+        pytest.param(inf, expected_string_inf, id="test_convert_html_subsripts_to_latex_inf"),
+        pytest.param(sup_extended, expected_string_sup_extended, id="test_convert_html_subsripts_to_latex_sup_extended"),
+        pytest.param(sub_extended, expected_string_sub_extended, id="test_convert_html_subsripts_to_latex_sub_extended"),
+        pytest.param(inf_extended, expected_string_inf_extended, id="test_convert_html_subsripts_to_latex_inf_extended"),
+        pytest.param(no_tags, no_tags, id="test_convert_html_subsripts_to_latex_no_tags"),
+    ],
+)
+def test_convert_html_subsripts_to_latex(test_input, expected):
+    output = convert_html_subsripts_to_latex(test_input)
+    assert output == expected
+
+
